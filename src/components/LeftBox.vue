fc7b36de09ab14919f9fa51f3a97ba299e66bc69
Mule <1533238648@qq.com>
2024-03-13 21:15:21 +0800

    update: 更新布局

---
+++ src/components/LeftBox.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+</script>
+
+<template>
+  <div class="left">
+    左盒子
+<!--    <HelloWorld msg="test"/>-->
+  </div>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+  .left {
+  }
+}
+</style>
---
+++ src/components/RightBox.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    右盒子
+  </div>
+</template>
+
+<style scoped>
+
+</style>
